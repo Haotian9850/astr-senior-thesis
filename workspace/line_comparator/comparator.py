19990854c2a04fde092741cc0d58f3a02a2d66cd
Haotian9850 <haotian9850@gmail.com>
2019-10-27 17:00:05 -0400

    updated molecular line comparator

--- workspace/line_comparator/comparator.py
+++ workspace/line_comparator/comparator.py
@@ -1,20 +1,34 @@
 import sys
+import os
 
 
-def compare(f1, f2):
+
+def compare_all(dir_name):
+    print(compare_multiple(os.listdir(dir_name), dir_name))
+
+
+def compare_multiple(line_files, dir_name):
+    result = read_line_file(line_files[0], dir_name)
+    for i in range(0, len(line_files) - 1):
+        result = compare_two(result, read_line_file(line_files[i + 1], dir_name))
+    return result
+
+
+def compare_two(line1, line2):
     result = set()
-    for line in read_line_file(f1):
-        if line in read_line_file(f2):
+    for line in line1:
+        if line in line2:
             result.add(line)
-    print(result)
+    return result
 
 
-def read_line_file(file_name):
-    with open(file_name) as f:
+def read_line_file(file_name, dir_name):
+    with open("{}/{}".format(dir_name, file_name)) as f:
         lines = f.readlines()
     return set([line.rstrip() for line in lines])
             
 
 
 if __name__ == "__main__":
-    compare("data/{}.txt".format(sys.argv[1]), "data/{}.txt".format(sys.argv[2]))
+    # compare_two("data/{}.txt".format(sys.argv[1]), "data/{}.txt".format(sys.argv[2]))
+    compare_all("data")
