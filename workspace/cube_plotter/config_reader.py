7d39c0d890a286916b83db2bf06b2a9815f01d10
Haotian9850 <haotian9850@gmail.com>
2019-11-11 20:52:40 -0500

    updated astronomy

--- workspace/cube_plotter/config_reader.py
+++ workspace/cube_plotter/config_reader.py
@@ -4,6 +4,4 @@ def read_config(config_name):
     result = dict()
     with open(config_name) as config:
         result = yaml.safe_load(config)
-    print(result)
-
-read_config("config_test.yaml")
+    return result
--- workspace/cube_plotter/slice_plotter_2d.py
+++ workspace/cube_plotter/slice_plotter_2d.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
-from astropy.wcs import WCS
-from astropy.io import fits
-from astropy.utils.data import get_pkg_data_filename
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import text
 import sys
 
+from config_reader import read_config
+
+
 matplotlib.rc("font", size=12)
 
 DATA_BASE_DIR = "/mnt/documents-local/ASTR4998/data/raw/"
+TAB_FILE_NAME = "testCubeStats.tab"
+
 C = 2.99792458E+8
 
 
@@ -22,26 +24,47 @@ def read_cube_spectrum_file(data_dir, package_name, file_name):
     return x, y
 
 
-def plot(data_dir, package_name, file_name):
-    x, y = read_cube_spectrum_file(data_dir, package_name, file_name)
-    plt.plot(x, y)
-    plt.show()
+
+def plot(data_dir, package_name, file_name, config_name, prefix):
+    print("-------- reading config YAML file... --------")
+    config = read_config(config_name)
+    for line in config["promising_lines"].keys():
+        print("-------- plotting line {}... --------".format(line))
+        line_info = config["promising_lines"][line]
+        plot_individual_line(
+            line_info[0],
+            line_info[1],
+            config["max_y"],
+            line_info[2],
+            line,
+            line[3],
+            data_dir, 
+            package_name,
+            file_name,
+            line_info[4],
+            prefix,
+            line_info[5]
+        )
+    print("-------- all lines are plotted --------")
 
 
-def plot_individual_line(start_chan, end_chan, freq, y_range, offset_size, name, data_dir, package_name, file_name):
+
+
+def plot_individual_line(start_chan, end_chan, y_range, offset_size, name, vpos, data_dir, package_name, file_name, formula, prefix, frequency):
     x, y = read_cube_spectrum_file(data_dir, package_name, file_name)
     offset = int((((end_chan - start_chan) // 2) * offset_size))
     x , y = x[max(0, start_chan - offset) : min(len(x), end_chan + offset)], y[max(0, start_chan - offset) : min(len(x), end_chan + offset)]
     plt.plot(x, y)
-    plt.axvline(x=find_peak_x(x, y), linewidth=1.0, color="r", linestyle=":")
-    plt.text(find_peak_x(x, y), y_range * 0.6, name, rotation=90)
+    plt.axvline(x=frequency, linewidth=1.0, color="r", linestyle=":")
+    plt.text(find_peak_x(x, y), y_range * 0.6, formula, rotation=90)
     plt.ylim(0, y_range)
     plt.ticklabel_format(useOffset=False)
     plt.xlabel("Frequency (GHz)")
     plt.ylabel("Peak / Noise")
-    plt.savefig("spw0_CH3CHO.png", dpi=300)
+    plt.savefig("{}_{}.png".format(prefix, formula), dpi=300)
     plt.show()
     
+
 
 def find_peak_x(x, y):
     return x[y.index(max(y))]
@@ -49,6 +72,12 @@ def find_peak_x(x, y):
 
 
 if __name__ == "__main__":
-    plot_individual_line(97, 124, 336.959, 60, 0.4, "Acetaldehyde", DATA_BASE_DIR, "SerpS_TC_spw0.pbcor_cutout_180_180_100_line.fits.admit", "testCubeStats.tab")
+    plot(
+        DATA_BASE_DIR,
+        "SerpS_TC_spw1.pbcor_cutout_180_180_100_line.fits.admit",
+        TAB_FILE_NAME,
+        "config_test.yaml",
+        "spw1"
+    )
 
 
